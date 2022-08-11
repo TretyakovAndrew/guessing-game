a496c27d82efae3391659b3ac563f3f019170022
Andrei Tretyakov <drobservis@gmail.com>
2022-08-11 12:49:08 +0300

    feat: finish guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.min + this.max) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.mid;
+        this.guess();
     }
 }
 
